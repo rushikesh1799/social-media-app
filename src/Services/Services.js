f5bfa994fa4c0a444b71e72c3f0e384e99d5f92f
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-06-21 12:18:17 +0530

    Update: Created new API calling functions.
    Created new API calling functions for bookmarks, likesCount and to add posts.

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -3,3 +3,98 @@ import axios from "axios";
 export const getAllUsers = async () => await axios.get("/api/users");
 
 export const getAllPosts = async () => await axios.get("/api/posts");
+
+export const getAllBookmarks = async (encodedToken) =>
+    await axios.get("/api/users/bookmark/", {
+        headers: {
+            authorization: encodedToken,
+        },
+    });
+
+export const likePostService = async ({ post, encodedToken }) => {
+    console.log(`/api/posts/like/${post._id}`);
+    return axios.post(
+        `/api/posts/like/${post._id}`,
+        {},
+        {
+            headers: {
+                authorization: encodedToken,
+            },
+        }
+    );
+};
+
+export const disLikePostService = async ({ post, encodedToken }) => {
+    return axios.post(
+        `/api/posts/dislike/${post._id}`,
+        {},
+        {
+            headers: {
+                authorization: encodedToken,
+            },
+        }
+    );
+};
+
+export const bookmarkPostService = async ({ post, encodedToken }) => {
+    return axios.post(
+        `/api/users/bookmark/${post._id}`,
+        {},
+        {
+            headers: {
+                authorization: encodedToken,
+            },
+        }
+    );
+};
+
+export const removeBookmarkPostService = async ({ post, encodedToken }) => {
+    return axios.post(
+        `/api/users/remove-bookmark/${post._id}`,
+        {},
+        {
+            headers: {
+                authorization: encodedToken,
+            },
+        }
+    );
+};
+
+export const addPost = async ({ postContent, encodedToken }) => {
+    return axios.post(
+        `/api/posts`,
+        {
+            postData: postContent,
+        },
+        {
+            headers: {
+                authorization: encodedToken,
+            },
+        }
+    );
+};
+
+export const followService = async (userid, encodedToken) => {
+    const response = await fetch(
+        `/api/users/follow/${userid}`,
+        {},
+        {
+            headers: {
+                authorization: encodedToken,
+            },
+        }
+    );
+    console.log(response);
+    // return axios.post(
+    //     `/api/users/follow/${userid}`,
+    //     {},
+    //     {
+    //         headers: {
+    //             authorization: encodedToken,
+    //         },
+    //     }
+    // );
+};
+
+// "username": "adarshbalika",
+// "password": "adarshBalika123",
