dc8dbeb20063efc65bc7b39b5da90a1752bce8f2
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-06-21 12:21:22 +0530

    Update: updated the contexts files.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,11 +4,15 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(localStorage.getItem("token"));
-    const [user, setUser] = useState(localStorage.getItem("user"));
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 
     // useEffect(() => {
     //     console.log("user", user);
     // }, [user]);
+
+    // useEffect(() => {
+    //     console.log("token", token);
+    // }, [token]);
 
     return (
         <AuthContext.Provider value={{ token, setToken, user, setUser }}>
--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,26 +1,47 @@
-import { createContext, useEffect, useReducer } from "react";
+import {
+    createContext,
+    useContext,
+    useEffect,
+    useReducer,
+    useState,
+} from "react";
 import { DataReducer } from "../reducers/DataReducer";
-import { getAllPosts, getAllUsers } from "../Services/Services";
-
+import {
+    addPost,
+    bookmarkPostService,
+    disLikePostService,
+    followService,
+    getAllBookmarks,
+    getAllPosts,
+    getAllUsers,
+    likePostService,
+    removeBookmarkPostService,
+} from "../Services/Services";
+import { AuthContext } from "./AuthContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 export const DataContext = createContext();
 
 const initialState = {
     users: [],
     posts: [],
+    category: "all",
+    filter: "",
+    bookmarks: [],
 };
 
 export const DataProvider = ({ children }) => {
     const [state, dispatch] = useReducer(DataReducer, initialState);
+    const [loading, setLoading] = useState(false);
+    const { user, token } = useContext(AuthContext);
 
     // destructuring of the initialState
-    const { users, posts } = state;
+    const { users, posts, category, bookmarks, filter } = state;
+    const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     console.log(posts);
-    // }, [posts]);
-    // useEffect(() => {
-    //     console.log(users);
-    // }, [users]);
+    useEffect(() => {
+        console.log("posts", posts);
+    }, [posts]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,21 +57,148 @@ export const DataProvider = ({ children }) => {
                 });
             }
 
-            const fetchAllPosts = await getAllPosts();
-            if (fetchAllPosts.status === 200 || fetchAllPosts.status === 201) {
-                // console.log(fetchAllPosts.data.posts);
-                dispatch({
-                    type: "GET_POSTS",
-                    payload: { posts: fetchAllPosts.data.posts },
-                });
-            }
+            const fetchBookmarksData = await getAllBookmarks(token);
+            // Need to work on these getAllBookmarks result data here
+            // console.log(fetchBookmarksData);
         };
+
         fetchData();
     }, []);
 
+    const fetchAllPosts = async () => {
+        const result = await getAllPosts();
+        if (result.status === 200 || result.status === 201) {
+            // console.log(fetchAllPosts.data.posts);
+            dispatch({
+                type: "GET_POSTS",
+                payload: { posts: result.data.posts },
+            });
+        }
+    };
+
+    useEffect(() => {
+        fetchAllPosts();
+    }, []);
+
+    const handleLike = async (post) => {
+        const result = await likePostService({
+            post: { ...post },
+            encodedToken: token,
+        });
+        console.log("Like Result", result);
+        if (result.status === 200 || result.status === 201) {
+            dispatch({
+                type: "ADD_LIKE",
+                payload: { posts: result.data.posts },
+            });
+        }
+    };
+    const handleDisLike = async (post) => {
+        const result = await disLikePostService({
+            post: { ...post },
+            encodedToken: token,
+        });
+        if (result.status === 200 || result.status === 201) {
+            dispatch({
+                type: "ADD_DISLIKE",
+                payload: { posts: result.data.posts },
+            });
+        }
+    };
+
+    const handleBookmark = async (post) => {
+        const result = await bookmarkPostService({
+            post: post,
+            encodedToken: token,
+        });
+        console.log("Bookmark Result:", result);
+        if (result.status === 200 || result.status === 201) {
+            dispatch({
+                type: "ADD_BOOKMARK",
+                payload: { bookmarks: result.data.bookmarks },
+            });
+        }
+    };
+
+    const handleRemoveBookmark = async (post) => {
+        const result = await removeBookmarkPostService({
+            post: { ...post },
+            encodedToken: token,
+        });
+        console.log("Remove Bookmark Result:", result);
+        if (result.status === 200 || result.status === 201) {
+            dispatch({
+                type: "REMOVE_BOOKMARK",
+                payload: { bookmarks: result.data.bookmarks },
+            });
+        }
+    };
+
+    const handleFollowUser = async (userID) => {
+        const result = await fetch(`/api/users/follow/${userID}`, {
+            method: "POST",
+            headers: {
+                authorization: token,
+            },
+        });
+        const data = await result.json();
+        console.log(data);
+    };
+
+    const handleAddPost = async (postContent) => {
+        const result = await addPost({
+            postContent: { content: postContent },
+            encodedToken: token,
+        });
+        dispatch({ type: "ADD_POSTS", payload: { posts: result.data.posts } });
+    };
+
+    // postData: { content: postContent },
+
+    const handleCategory = (e) => {
+        // console.log(e.target.value.toLowerCase());
+        dispatch({
+            type: "SET_CATEGORY",
+            payload: e.target.value.toLowerCase(),
+        });
+    };
+
+    const handleFilters = (e) => {
+        // console.log(e.target.value);
+        dispatch({
+            type: "SET_FILTER",
+            payload: e.target.value,
+        });
+    };
+
     return (
-        <DataContext.Provider value={{ users, posts }}>
+        <DataContext.Provider
+            value={{
+                users,
+                posts,
+                loading,
+                category,
+                filter,
+                bookmarks,
+                setLoading,
+                handleLike,
+                handleDisLike,
+                handleBookmark,
+                handleRemoveBookmark,
+                handleFollowUser,
+                handleCategory,
+                handleFilters,
+                handleAddPost,
+            }}
+        >
             {children}
         </DataContext.Provider>
     );
 };
+
+// {
+//     "username": "pratik0077",
+//     "password":"asd"
+//     }
+
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI3YmUxNjQyZi04Mjk5LTRhMTgtYWY4Yi1lM2M0ZGU5M2I2MWEiLCJ1c2VybmFtZSI6InByYXRpazAwNzcifQ.2BmxqEWdpHUrpMiuoxi3kP2nclAUfmp0G-9VQFIzIxs"
