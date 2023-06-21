4bcaf9793c1b08c7fa0e41657e65929acb127775
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-06-21 12:22:09 +0530

    Create RequiresAuth.jsx
    Created RequiresAuth.jsx to hide the private routes.

---
+++ src/Component/RequiresAuth/RequiresAuth.jsx
@@ -0,0 +1,22 @@
+import React, { useContext, useEffect } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
+import { Navigate, useLocation } from "react-router-dom";
+
+const RequiresAuth = ({ children }) => {
+    const { token } = useContext(AuthContext);
+
+    const location = useLocation();
+
+    // useEffect(() => {
+    //     console.log(token);
+    // }, [token]);
+
+    return token ? (
+        children
+    ) : (
+        <Navigate to="/login" state={{ from: location }} />
+    );
+};
+
+export default RequiresAuth;
