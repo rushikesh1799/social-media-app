5848848f6a4c2dcb8dfccf71cb4a6863e97b5b12
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-07-10 09:26:29 +0530

    Update: Added image add to post functionality

--- src/Component/CreateNewPost/CreateNewPost.jsx
+++ src/Component/CreateNewPost/CreateNewPost.jsx
@@ -8,27 +8,56 @@ const CreateNewPost = () => {
     const [postContent, setPostContent] = useState("");
     const { handleAddPost } = useContext(DataContext);
 
+    const [previewSource, setPreviewSource] = useState("");
     // useEffect(() => {
     //     console.log(postContent);
     // }, [postContent]);
 
+    const uploadImage = (e) => {
+        const file = e.target.files[0];
+        const imgUrl = URL.createObjectURL(file);
+        setPreviewSource(imgUrl);
+    };
+
     return (
         <div className="new__post__container">
             <div className="create-a-post-wrapper">
-                <textarea
-                    className="new__post__input"
-                    placeholder="What's happening?"
-                    value={postContent}
-                    onChange={(e) => setPostContent(e.target.value)}
-                ></textarea>
+                <div className="text-content-container">
+                    <textarea
+                        className="new__post__input"
+                        placeholder="What's happening?"
+                        value={postContent}
+                        onChange={(e) => setPostContent(e.target.value)}
+                    ></textarea>
+                </div>
+                {previewSource && (
+                    <div className="media-container">
+                        <img src={previewSource} className="input__img" />
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                )}
+
                 <div className="new__post__footer">
-                    <i className="fa fa-picture-o" aria-hidden="true"></i>
+                    <label htmlFor="file">
+                        <i className="fa fa-picture-o" aria-hidden="true"></i>
+                    </label>
+
+                    <input
+                        style={{ display: "none" }}
+                        type="file"
+                        id="file"
+                        onChange={uploadImage}
+                        accept=".png,.jpeg,.jpg"
+                    ></input>
+
                     <button
                         className="post__btn"
                         onClick={() => {
-                            handleAddPost(postContent);
+                            handleAddPost(postContent, previewSource);
                             setPostContent("");
+                            setPreviewSource("");
                         }}
+                        disabled={postContent.length === 0 ? true : false}
                     >
                         Post
                     </button>
