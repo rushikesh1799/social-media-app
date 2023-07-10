fe1782828645d36a53a94484b43267c0129fd81c
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-07-10 16:38:50 +0530

    Update: cleared a bug

--- src/Component/CreateNewPost/CreateNewPost.jsx
+++ src/Component/CreateNewPost/CreateNewPost.jsx
@@ -33,7 +33,7 @@ const CreateNewPost = () => {
                 {previewSource && (
                     <div className="media-container">
                         <img src={previewSource} className="input__img" />
-                        <div onClick={() => setPreviewSource(() => "")}>
+                        <div onClick={() => setPreviewSource("")}>
                             <i className="fa fa-times" aria-hidden="true"></i>
                         </div>
                     </div>
@@ -57,7 +57,7 @@ const CreateNewPost = () => {
                         onClick={() => {
                             handleAddPost(postContent, previewSource);
                             setPostContent("");
-                            setPreviewSource(() => "");
+                            setPreviewSource("");
                         }}
                         disabled={postContent.length === 0 ? true : false}
                     >
